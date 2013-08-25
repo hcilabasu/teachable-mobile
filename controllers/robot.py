91712b88897e4cf8fa14eae21e6c2834c84d988a
hcilabasu <hcilabasu@gmail.com>
2013-08-25 15:17:32 -0700

    Improvements on robot code

--- controllers/robot.py
+++ controllers/robot.py
@@ -3,7 +3,15 @@ from RobotController import RobotController
 from threading import Thread
 import time
 import math
+import config
+import urllib
+from gluon.contrib.websocket_messaging import websocket_send
 
+__current_user_name = config.TORNADO_USER
+__current_ip = config.TORNADO_IP
+__socket_port = config.TORNADO_PORT
+__key = config.TORNADO_KEY
+__socket_group_name = config.TORNADO_GROUP_ROBOT
 
 direction = 0
 position = (0,0)
@@ -21,14 +29,11 @@ rc = RobotController()
 recursion_cutoff = 5
 
 def index():
-	return dict()
+	return dict(ip=__current_ip, port=__socket_port, group_name=__socket_group_name)
 
 def update_direction():
 	RobotController.d = request.vars['d']
 	return RobotController.d
-
-def get_direction():
-	return int(float(RobotController.d))
 
 def see_position():
 	return str(session.position)
@@ -99,9 +104,9 @@ def __turn_to(angle, backwards, recursion):
 			d += 360
 	
 		if d > 0:
-			turn_left()
+			turn_left(__within_threshold(angle, angle_threshold))
 		elif d < 0:
-			turn_right()
+			turn_right(__within_threshold(angle, angle_threshold))
 		while True:
 			# time.sleep(0.01)
 			if __within_threshold(angle, angle_threshold):
@@ -171,22 +176,27 @@ def __move_to(x, y, backwards, recursion):
 		if math.fabs(c_x - x) > distance_threshold or math.fabs(c_y - y) > distance_threshold:
 			__move_to(x, y, backwards, recursion + 1)
 
+url = 'http://169.254.67.33:59125/process.wav?INPUT_TYPE=TEXT&OUTPUT_TYPE=AUDIO&INPUT_TEXT={0}&VOICE_SELECTIONS=cmu-rms-hsmm%20en_US%20male%20hmm&AUDIO_OUT=AIFF_FILE&LOCALE=en_US&VOICE=cmu-rms-hsmm&AUDIO=AIFF_FILE'
+url = 'http://169.254.67.33:59125/process.wav?INPUT_TEXT={0}&INPUT_TYPE=TEXT&OUTPUT_TYPE=AUDIO&AUDIO=WAVE_FILE&LOCALE=en_US'
+# url = 'http://169.254.67.33:8000/mobileinterface/static/audio/welcome.wav'
+
+def speak():
+	sentence = urllib.quote(request.vars['sentence'])
+	tts_url = url.format(sentence)
+	websocket_send('http://' + 'localhost' + ':' + __socket_port, tts_url, 'mykey', 'robot')
 
 
-
-
-
-
-
+def a():
+	return A('asdjlkashdjkashdlashd', _href="http://169.254.67.33:59125/process?INPUT_TYPE=TEXT&OUTPUT_TYPE=AUDIO&INPUT_TEXT=Welcome%20to%20the%20world%20of%20speech%20synthesis!%0A&OUTPUT_TEXT=&effect_Volume_selected=&effect_Volume_parameters=amount%3A2.0%3B&effect_Volume_default=Default&effect_Volume_help=Help&effect_TractScaler_selected=&effect_TractScaler_parameters=amount%3A1.5%3B&effect_TractScaler_default=Default&effect_TractScaler_help=Help&effect_F0Scale_selected=&effect_F0Scale_parameters=f0Scale%3A2.0%3B&effect_F0Scale_default=Default&effect_F0Scale_help=Help&effect_F0Add_selected=&effect_F0Add_parameters=f0Add%3A50.0%3B&effect_F0Add_default=Default&effect_F0Add_help=Help&effect_Rate_selected=&effect_Rate_parameters=durScale%3A1.5%3B&effect_Rate_default=Default&effect_Rate_help=Help&effect_Robot_selected=&effect_Robot_parameters=amount%3A100.0%3B&effect_Robot_default=Default&effect_Robot_help=Help&effect_Whisper_selected=&effect_Whisper_parameters=amount%3A100.0%3B&effect_Whisper_default=Default&effect_Whisper_help=Help&effect_Stadium_selected=&effect_Stadium_parameters=amount%3A100.0&effect_Stadium_default=Default&effect_Stadium_help=Help&effect_Chorus_selected=&effect_Chorus_parameters=delay1%3A466%3Bamp1%3A0.54%3Bdelay2%3A600%3Bamp2%3A-0.10%3Bdelay3%3A250%3Bamp3%3A0.30&effect_Chorus_default=Default&effect_Chorus_help=Help&effect_FIRFilter_selected=&effect_FIRFilter_parameters=type%3A3%3Bfc1%3A500.0%3Bfc2%3A2000.0&effect_FIRFilter_default=Default&effect_FIRFilter_help=Help&effect_JetPilot_selected=&effect_JetPilot_parameters=&effect_JetPilot_default=Default&effect_JetPilot_help=Help&HELP_TEXT=&exampleTexts=&VOICE_SELECTIONS=cmu-slt-hsmm%20en_US%20female%20hmm&AUDIO_OUT=WAVE_FILE&LOCALE=en_US&VOICE=cmu-slt-hsmm&AUDIO=WAVE_FILE")
 
 def move_forward():
 	rc.get_robot().forwardArrow()
 
-def turn_left():
-	rc.get_robot().leftArrow()
+def turn_left(low_speed):
+	rc.get_robot().leftArrow(low_speed)
 
-def turn_right():
-	rc.get_robot().rightArrow()
+def turn_right(low_speed):
+	rc.get_robot().rightArrow(low_speed)
 
 def move_backward():
 	rc.get_robot().backwardArrow()
@@ -196,3 +206,6 @@ def stop():
 
 def __get_position():
 	return gateway.getCurrentPositionAsArray()
+
+def get_direction():
+	return int(float(RobotController.d))

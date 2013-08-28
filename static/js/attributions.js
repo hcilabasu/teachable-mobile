71ffa9adbe92f74a4dd67d5bbe4d6650a037d21f
hcilabasu <hcilabasu@gmail.com>
2013-08-27 21:18:59 -0700

    Finishing first round of attributions
    Still waiting for more detailed info from Kasia, though

--- static/js/attributions.js
+++ static/js/attributions.js
@@ -1,14 +1,13 @@
 var Attributions = function(){
 
-	var attributions = ['i','you','we'];
 	var emotions = {
 		'desirable' : {
-			'success' : attributions.slice(0),
-			'failure' : attributions.slice(0)
+			'success' : [{'dim':'i','attr':'pride'},{'dim':'you','attr':'gratitude'},{'dim':'we','attr':'pride'}], // Ability
+			'failure' : [{'dim':'i','attr':'guilt'},{'dim':'you','attr':'frustration'},{'dim':'we','attr':'guilt'}] //  Effort
 		},	
 		'undesirable' : {
-			'success' : attributions.slice(0),
-			'failure' : attributions.slice(0)
+			'success' : [{'dim':'i','attr':'pride'},{'dim':'you','attr':'gratitude'},{'dim':'we','attr':'pride'}], // Effort, luck
+			'failure' : [{'dim':'i','attr':'shame'},{'dim':'you','attr':'pity'},{'dim':'we','attr':'shame'}] // Lack of ability
 		}
 	};
 
@@ -18,17 +17,24 @@ var Attributions = function(){
 
 	var makeAttribution = function(attr){
 		var dimensions = emotions[attr.des_attribution][attr.outcome]
-		var finalAttribution = attr.des_attribution + '_' + attr.outcome + '_' + dimensions[0];
+		var finalAttribution = attr.des_attribution + '_' + attr.outcome + '_' + dimensions[0].dim;
+		// Set facial expression
+		$("body").removeClass().addClass(dimensions[0].attr);
 		// Rotating reactions
 		emotions[attr.des_attribution][attr.outcome] = rotate(dimensions, 1);
 		// load sound
 		AUDIO.loadSound(finalAttribution);
-		// Attach handler
-		AUDIO.addFinishListener(finalAttribution, function(){
-			$("body").removeClass("talking");
-		});
+		// Attach handlers
 		AUDIO.addStartListener(finalAttribution, function(){
+			// When robot starts to speak
 			$("body").addClass("talking");
+		});
+		AUDIO.addFinishListener(finalAttribution, function(){
+			// When robot finishes speaking
+			$("body").removeClass("talking");
+			window.setTimeout(function(){
+				$("body").removeClass().addClass("neutral");
+			}, 3000);
 		});
 		// play sound
 		AUDIO.play(finalAttribution);

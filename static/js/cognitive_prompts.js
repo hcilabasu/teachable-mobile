a8e8136692da20f9eb9251c19821dd10b76bef9f
HCI Lab ASU <hcilabasu@gmail.com>
2014-02-19 23:58:46 -0700

    adding mobile condition prompt dismiss button

--- static/js/cognitive_prompts.js
+++ static/js/cognitive_prompts.js
@@ -43,7 +43,7 @@ var CognitivePrompts = function(){
 		}
 	}
 
-	var makePrompt = function(prompt){
+	var makePrompt = function(prompt, condition){
 		// TEMPORARILY HARDCODING PROMPTS, SHOULD FIX THIS LATER
         var prompts = '[ {"problem_num":540, "problem_state":"end", "orientation":"", "text":"Are you ready to teach me geometry?", "sound_file":"0.aiff", "another_prompt":"false"}, {"problem_num":541, "problem_state":"beg", "orientation":"", "text":"In (4, 0), does the 4 tell me which way to move on the x-axis or on the y-axis?", "sound_file":"1.aiff", "another_prompt":"false"}, {"problem_num":541, "problem_state":"end", "orientation":"","text":"Neat! Now I know that the first number tells me to go left or right on the x-axis!", "sound_file":"2.aiff", "another_prompt":"true"}, {"problem_num":541, "problem_state":"end", "orientation":"", "text":"Hey can you show me where 4 is on the x-axis again?", "sound_file":"3.aiff", "another_prompt":"false"}, {"problem_num":542, "problem_state":"beg", "orientation":"", "text":"I have a question. Is the x-axis the one that runs left to right?", "sound_file":"4.aiff", "another_prompt":"false"}, {"problem_num":542, "problem_state":"mid", "orientation":"180", "text":"I turned so now I\'m facing left on the x-axis!", "sound_file":"5.aiff", "another_prompt":"false"}, {"problem_num":542, "problem_state":"end", "orientation":"", "text":"I can\'t remember which way the y-axis goes! Do you? Can you walk along the y-axis for me?", "sound_file":"6.aiff", "another_prompt":"true"}, {"problem_num":542, "problem_state":"end", "orientation":"", "text":"Cool! I learned that the y-axis runs from top to bottom across the cartesian plane!", "sound_file":"7.aiff", "another_prompt":"false"}, {"problem_num":543, "problem_state":"beg", "orientation":"", "text":"I’m trying to remember where all the x’s are positive. Is it to the left or the right of the y-axis?", "sound_file":"8.aiff", "another_prompt":"true"}, {"problem_num":543, "problem_state":"beg", "orientation":"", "text":"Hmmm our x coordinate is 1. Does this mean we should move left or right along the x-axis?", "sound_file":"9.aiff", "another_prompt":"false"}, {"problem_num":543, "problem_state":"end", "orientation":"", "text":"Where are the y’s all positive? Is it below or above the x-axis?", "sound_file":"10.aiff", "another_prompt":"true"}, {"problem_num":543, "problem_state":"end", "orientation":"", "text":"I forgot something... Can you walk around the area where the y’s are all positive?", "sound_file":"11.aiff", "another_prompt":"false"}, {"problem_num":544, "problem_state":"beg", "orientation":"", "text":"coordinate is -2. Does this mean we should move left or right along the x-axis?", "sound_file":"12.aiff", "another_prompt":"false"}, {"problem_num":544, "problem_state":"mid", "orientation":"270", "text":"I have a feeling the y\'s are all negative below the x-axis…", "sound_file":"12.aiff", "another_prompt":"true"}, {"problem_num":544, "problem_state":"mid", "orientation":"270", "text":"If we walk to the bottom edge of the cartesian plane, are the y’s positive or negative?", "sound_file":"12.aiff", "another_prompt":"false"}, {"problem_num":544, "problem_state":"end", "orientation":"", "text":"I forgot where the x’s are all negative! Can you show me?", "sound_file":"12.aiff", "another_prompt":"false"}]';
         prompts = prompts.replace(/&#x27;/g, "'");
@@ -138,6 +138,7 @@ var CognitivePrompts = function(){
 	}
 
 	var displayTriggeredPrompt = function(prompt, condition) {
+		console.log(condition);
 		// TEMPORARILY HARDCODING PROMPTS, SHOULD FIX THIS LATER
         var prompts = '[ {"problem_num":540, "problem_state":"end", "orientation":"", "text":"Are you ready to teach me geometry?", "sound_file":"0.aiff", "another_prompt":"false"}, {"problem_num":541, "problem_state":"beg", "orientation":"", "text":"In (4, 0), does the 4 tell me which way to move on the x-axis or on the y-axis?", "sound_file":"1.aiff", "another_prompt":"false"}, {"problem_num":541, "problem_state":"end", "orientation":"","text":"Neat! Now I know that the first number tells me to go left or right on the x-axis!", "sound_file":"2.aiff", "another_prompt":"true"}, {"problem_num":541, "problem_state":"end", "orientation":"", "text":"Hey can you show me where 4 is on the x-axis again?", "sound_file":"3.aiff", "another_prompt":"false"}, {"problem_num":542, "problem_state":"beg", "orientation":"", "text":"I have a question. Is the x-axis the one that runs left to right?", "sound_file":"4.aiff", "another_prompt":"false"}, {"problem_num":542, "problem_state":"mid", "orientation":"180", "text":"I turned so now I\'m facing left on the x-axis!", "sound_file":"5.aiff", "another_prompt":"false"}, {"problem_num":542, "problem_state":"end", "orientation":"", "text":"I can\'t remember which way the y-axis goes! Do you? Can you walk along the y-axis for me?", "sound_file":"6.aiff", "another_prompt":"true"}, {"problem_num":542, "problem_state":"end", "orientation":"", "text":"Cool! I learned that the y-axis runs from top to bottom across the cartesian plane!", "sound_file":"7.aiff", "another_prompt":"false"}, {"problem_num":543, "problem_state":"beg", "orientation":"", "text":"I’m trying to remember where all the x’s are positive. Is it to the left or the right of the y-axis?", "sound_file":"8.aiff", "another_prompt":"true"}, {"problem_num":543, "problem_state":"beg", "orientation":"", "text":"Hmmm our x coordinate is 1. Does this mean we should move left or right along the x-axis?", "sound_file":"9.aiff", "another_prompt":"false"}, {"problem_num":543, "problem_state":"end", "orientation":"", "text":"Where are the y’s all positive? Is it below or above the x-axis?", "sound_file":"10.aiff", "another_prompt":"true"}, {"problem_num":543, "problem_state":"end", "orientation":"", "text":"I forgot something... Can you walk around the area where the y’s are all positive?", "sound_file":"11.aiff", "another_prompt":"false"}, {"problem_num":544, "problem_state":"beg", "orientation":"", "text":"coordinate is -2. Does this mean we should move left or right along the x-axis?", "sound_file":"12.aiff", "another_prompt":"false"}, {"problem_num":544, "problem_state":"mid", "orientation":"270", "text":"I have a feeling the y\'s are all negative below the x-axis…", "sound_file":"12.aiff", "another_prompt":"true"}, {"problem_num":544, "problem_state":"mid", "orientation":"270", "text":"If we walk to the bottom edge of the cartesian plane, are the y’s positive or negative?", "sound_file":"12.aiff", "another_prompt":"false"}, {"problem_num":544, "problem_state":"end", "orientation":"", "text":"I forgot where the x’s are all negative! Can you show me?", "sound_file":"12.aiff", "another_prompt":"false"}]';
         prompts = prompts.replace(/&#x27;/g, "'");
@@ -159,8 +160,9 @@ var CognitivePrompts = function(){
 			log("", {"type":"prompt","parameter":prompts[currentPromptIndex].text, "initial" : "", "final" : ""});
 
 
-			if(condition == "Mobile")
+			if(condition != "Mobile")
 			{
+				console.log("a")
 				$("#record").text('REC');
 				$("#record").fadeIn('slow');
 				$("#record").off('click');
@@ -217,6 +219,7 @@ var CognitivePrompts = function(){
 			}
 			else
 			{
+				console.log("b");
 				$("#record").text('Dismiss');
 				$("#record").fadeIn('slow');
 				$("#record").off('click');
@@ -242,27 +245,28 @@ var CognitivePrompts = function(){
 							displayTriggeredPrompt(prompt);
 							promptsFinished = true;
 						}
+				});
 				$("#record").off('click');
-				}
+				
 			}
-		});
 		
-		AUDIO.addFinishListener("promptSound", function(){
-			// When robot finishes speaking
-			$("body").removeClass("talking");
-		});
-		// play sound
-		window.setTimeout(function(){
-			AUDIO.play("promptSound");
-		}, 3000);
+			AUDIO.addFinishListener("promptSound", function(){
+				// When robot finishes speaking
+				$("body").removeClass("talking");
+			});
+			// play sound
+			window.setTimeout(function(){
+				AUDIO.play("promptSound");
+			}, 3000);
 
-		console.log("prompt: " + currentPromptIndex + ", problem: " + localProblemIndex);
+			console.log("prompt: " + currentPromptIndex + ", problem: " + localProblemIndex);
 
-		// TODO separate into a function
-		$.ajax({
-			url: 'robot/prompt_was_made'
-		});
-	}
+			// TODO separate into a function
+			$.ajax({
+				url: 'robot/prompt_was_made'
+			});
+		}
+	)}
 
 	return {
 		doPromptAction : doPromptAction
